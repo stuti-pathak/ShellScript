9e77ee4934dc97603ce899a767755bdc6d6cf3da
stuti-pathak <stuti.pathak@bridgelabz.com>
2022-03-11 11:44:48 +0530

    Added: calculating emp wage for the month

--- empWage.sh
+++ empWage.sh
@@ -3,15 +3,21 @@
 isFullTime=1
 isPartTime=2
 empRatePerHr=20
-empCheck=$((RANDOM%3))
+maxWorkingDays=20
+totalSalary=0
 
-case $empCheck in 
-	$isFullTime) empHrs=8
-					;;
-	$isPartTime) empHrs=4
-					;;
-				*) empHrs=0
-					;;
-esac
-salary=$(($empRatePerHr*$empHrs))
-echo "employee salary: "$salary
+for (( days=1;days<=$maxWorkingDays;days++ ))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in 
+		$isFullTime) empHrs=8
+						;;
+		$isPartTime) empHrs=4
+						;;
+					*) empHrs=0
+						;;
+	esac
+	salary=$(($empRatePerHr*$empHrs))
+	totalSalary=$(($totalSalary+$salary))
+done
+echo "employee total salary for the month: "$totalSalary
